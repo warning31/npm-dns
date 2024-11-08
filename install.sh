fd8b93c1788f831f693b53bc08c13bc04b9e01a3
warning31 <mustafaavci33@gmail.com>
2024-11-08 21:51:12 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -144,6 +144,7 @@ services:
   app:
     image: 'jc21/nginx-proxy-manager'
     restart: unless-stopped
+    network_mode: host
     ports:
       # These ports are in format <host-port>:<container-port>
       - '80:80' # Public HTTP Port
